2c53cd8e94d7d9e58b9ee8db50d6996b65cb0418
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-10-11 21:03:49 +0700

    fix: navbar and home page

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -9,10 +9,10 @@ import Avatar from '@/components/Elements/Avatar/Avatar';
 export default function Home() {
   return (
     <Main>
-      <div className='flex w-full h-full justify-center pr-16'>
+      <div className='flex w-full h-full justify-center xl:pr-20 2xl:pr-24'>
         {/* Text */}
         <div className='relative flex w-full h-full'>
-          <div className='w-full static xl:absolute top-0 flex flex-col gap-8'>
+          <div className='w-full static xl:absolute top-0 flex flex-col gap-8 text-center md:text-start'>
             <motion.h1
               initial={{ y: -20, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -29,10 +29,11 @@ export default function Home() {
               transition={{ ease: 'easeInOut', duration: 1.75 }}
               className='md:max-w-md xl:max-w-4xl mx-auto xl:mx-0 text-sm md:text-base text-justify'
             >
+              Hai 👋🏼👋🏼👋🏼... my name is Yerikho William Tasilima.
               I am a dedicated web developer currently immersed in a full-stack
               bootcamp, where I'm honing my skills in both front-end and
               back-end technologies. With a strong foundation in HTML, CSS,
-              JavaScript, and popular frameworks, I am committed to delivering
+              JavaScript, I am committed to delivering
               innovative solutions that meet the needs of users and clients
               alike.
             </motion.p>
@@ -55,7 +56,7 @@ export default function Home() {
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ ease: 'easeInOut', duration: 1.75 }}
-          className='relative w-full lg:w-[520px] lg:h-[320px] xl:w-[320px] xl:h-[320px] lg:right-0 hidden lg:block'
+          className='relative lg:w-[520px] lg:h-[320px] xl:w-[320px] xl:h-[320px] lg:right-0 hidden lg:block'
         >
           <Avatar />
         </motion.div>
